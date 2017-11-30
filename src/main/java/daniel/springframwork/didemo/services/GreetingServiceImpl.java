ad815c967d0c88c32c61c0e249ef5074f59a62f2
chitown1977 <daniel.tominov@gmail.com>
2017-11-29 18:26:38 -0800

    Initial commit

---
+++ src/main/java/daniel/springframwork/didemo/services/GreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package daniel.springframwork.didemo.services;
+
+/**
+ * @author Daniel Tominov on 11/29/2017 6:21 PM
+ **/
+public class GreetingServiceImpl implements GreetingService {
+    public String hello = "Hello Everyone";
+
+    @Override
+    public String sayGreeting() {
+        return hello;
+    }
+}
